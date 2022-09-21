3e1ccc910360687d8cc04ff5cde6d5a17a7c088f
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2022-09-21 16:41:37 +0200

    fix dockerfile

--- elyra/build.sh
+++ elyra/build.sh
@@ -1,4 +1,4 @@
 export IMAGE_VERSION="0.2"
 docker build -t elyra-ce-bfh:$IMAGE_VERSION .
 docker tag elyra-ce-bfh:$IMAGE_VERSION romeokienzler/elyra-ce-bfh:$IMAGE_VERSION
-docker push romeokienzler/elyra-ce:-bfh$IMAGE_VERSION
+docker push romeokienzler/elyra-ce-bfh:$IMAGE_VERSION
